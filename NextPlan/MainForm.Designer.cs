8407d6be08e05c067b2b20d6e765e82191d2dbc2
iserain <iserainn@gmail.com>
2018-07-30 00:18:48 +0900

    project import

---
+++ NextPlan/MainForm.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace NextPlan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mTimer = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // mTimer
+            // 
+            this.mTimer.Interval = 1;
+            this.mTimer.Tick += new System.EventHandler(this.mTimer_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(0, 0);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(81, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(760, 280);
+            this.listBox1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(853, 304);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer mTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ NextPlan/MainForm.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace NextPlan
+{
+    public partial class MainForm : Form
+    {
+        [DllImport("user32.dll")]
+        private static extern uint PostMessage(IntPtr hwnd, int wMsg, int wParam, int lParam);
+        [DllImport("user32.dll")]
+        private static extern IntPtr FindWindowEx(IntPtr hWnd1, int hWnd2, string lpsz1, string lpsz2);
+
+        private const int WM_LBUTTONDOWN = 0x0201;
+        private const int WM_LBUTTONUP = 0x0202;
+        private const int WM_RBUTTONDOWN = 0X0204;
+        private const int WM_RBUTTONUP = 0x0205;
+
+
+        private ChromiumWebBrowser mBrowser;
+        private CefSettings mSettings;
+        private IntPtr mHandle;
+
+        private Random r = new Random();
+        private string EthAddress = "f18e249d106c437cb6b4299f9a64d3a86cb25a6abf80ef5745b612153835f92d";
+
+
+        private int Step = 0;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mSettings = new CefSettings();
+            Cef.Initialize(mSettings);
+
+            mBrowser = new ChromiumWebBrowser("https://www.myetherwallet.com/#view-wallet-info");
+            mBrowser.Dock = DockStyle.None;
+            mBrowser.Size = new Size(1920, 1080);
+            mBrowser.Location = new Point(0, 0);
+
+            Controls.Add(mBrowser);
+            
+            
+        }
+
+        private int LParam(int LoWord, int HiWord)
+        {
+            HiWord -= 23;
+            return (int)((HiWord << 16) | (LoWord & 0xFFFF));
+        }
+
+        private void LClick(int x, int y, int next)
+        {
+            PostMessage(mHandle, WM_LBUTTONDOWN, 1, LParam(x, y));
+            PostMessage(mHandle, WM_LBUTTONUP, 1, LParam(x, y));
+
+            mTimer.Interval = next;
+        }
+        private void RClick(int x, int y, int next)
+        {
+            PostMessage(mHandle, WM_RBUTTONDOWN, 1, LParam(x, y));
+            PostMessage(mHandle, WM_RBUTTONUP, 1, LParam(x, y));
+
+            mTimer.Interval = next;
+        }
+
+        private void GetHandle()
+        {
+            mHandle = IntPtr.Zero;
+            mHandle = FindWindowEx(Handle, 0, "WindowsForms10.Window.8.app.0.141b42a_r10_ad1", null);
+            mHandle = FindWindowEx(mHandle, 0, "CefBrowserWindow", null);
+            mHandle = FindWindowEx(mHandle, 0, "Chrome_WidgetWin_0", null);
+            mHandle = FindWindowEx(mHandle, 0, "Chrome_RenderWidgetHostHWND", null);
+            
+            Text = mHandle == IntPtr.Zero ? "Not Found" : "Handle: " + mHandle;
+        }
+
+        private void GetRandomKey()
+        {
+            mTimer.Enabled = false;
+
+            EthAddress = string.Empty;
+            int tmp = 0;
+
+            for (int i = 0; i < 64; i++)
+            {
+                tmp = r.Next(0, 16);
+                switch (tmp)
+                {
+                    case 10:
+                        EthAddress += "a";
+                        break;
+                    case 11:
+                        EthAddress += "b";
+                        break;
+                    case 12:
+                        EthAddress += "c";
+                        break;
+                    case 13:
+                        EthAddress += "d";
+                        break;
+                    case 14:
+                        EthAddress += "e";
+                        break;
+                    case 15:
+                        EthAddress += "f";
+                        break;
+                    default:
+                        EthAddress += "" + tmp;
+                        break;
+                }
+
+            }
+            Clipboard.Clear();
+            Clipboard.SetData(DataFormats.Text, (Object)EthAddress);
+
+            mTimer.Interval = 100;
+            mTimer.Enabled = true;
+        }
+
+        private void mTimer_Tick(object sender, EventArgs e)
+        {
+            switch (Step)
+            {
+                case 1:
+                    LClick(30, 30, 500);
+                    break;
+                case 2:
+                    LClick(185, 642, 100);
+                    break;
+                case 3:
+                    LClick(736, 616, 100);
+                    break;
+                case 4:
+                    GetRandomKey();
+                    break;
+                case 5:
+                    RClick(736, 616, 250);
+                    break;
+                case 6:
+                    LClick(785, 750, 100);
+                    break;
+                case 7:
+                    LClick(765, 726, 4000);
+                    break;
+                case 8:
+                    mTimer.Enabled = false;
+                    GetSourceTestAsync();
+                    break;
+                case 9:
+                    LClick(109, 231,250);
+                    break;
+                case 10:
+                    RClick(736, 616, 250);
+                    break;
+                case 11:
+                    LClick(777, 777, 100);
+                    break;
+                case 12:
+                    Step = 3;
+                    break;
+            }
+
+
+            Step += 1;
+        }
+
+        private async Task GetSourceTestAsync()
+        {
+            List<string> FrameList = new List<string>();
+            FrameList = mBrowser.GetBrowser().GetFrameNames();
+            //Text = "out: " + FrameList.Count;
+
+
+            //mBrowser.GetBrowser().GetFrame(FrameList[0]).GetSource();
+            //MessageBox.Show(str.ToString());
+            string source = await mBrowser.GetMainFrame().GetSourceAsync();
+            if (source.IndexOf("Account Balance") > 0)
+            {
+                source = source.Substring(source.IndexOf("Account Balance") + 130);
+                source = source.Substring(0, source.IndexOf("</span>"));
+                //Text = source;
+                /*
+                if (double.Parse(source) > 0)
+                {
+                    
+                }
+                */
+                listBox1.Items.Add(EthAddress + " - " + source + " ETH");
+            }
+
+
+            mTimer.Interval = 100;
+            mTimer.Enabled = true;
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GetHandle();
+
+            Step = 1;
+            mTimer.Interval = 200;
+            mTimer.Enabled = true;
+        }
+    }
+}
